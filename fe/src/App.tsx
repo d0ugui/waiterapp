e87737efb7968a34bead959c481e807cb948ec09
Douglas Oliveira <douglaspo_97@outlook.com>
2022-11-17 11:08:10 -0300

    chore: init project, config linters and create header component

---
+++ fe/src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header';
+import { GlobalStyles } from './styles/globalStyles';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
+
---
+++ fe/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Content } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <div className="page-details">
+          <h1>Pedidos</h1>
+          <h2>Acompanhe os pedidos dos clientes</h2>
+        </div>
+
+        <img src={logo} alt="WaiterAPP" />
+      </Content>
+    </Container>
+  );
+}
