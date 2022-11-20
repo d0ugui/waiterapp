cfcb3826e94552f36ca34d218a034af139d97642
Douglas Oliveira <douglaspo_97@outlook.com>
2022-11-20 20:33:43 -0300

    fix: set-headers to fix cors error

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,6 +10,14 @@ mongoose.connect('mongodb://root:root@localhost:27017')
     const app = express();
     const port = 3001;
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
+
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
